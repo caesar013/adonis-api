a2806a2501b138affeba64b47341c4aba77389fd
cαεsαɾ <91421259+caesar013@users.noreply.github.com>
2024-08-06 11:56:54 +0700

    [feat] add migrations

--- database/migrations/1722850514865_create_users_table.ts
+++ database/migrations/1722850514865_create_users_table.ts
@@ -4,11 +4,13 @@ export default class extends BaseSchema {
   protected tableName = 'users'
 
   async up() {
+    this.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').notNullable()
-      table.string('full_name').nullable()
+      table.uuid('id').notNullable().primary()
+      table.string('name', 80).notNullable()
       table.string('email', 254).notNullable().unique()
       table.string('password').notNullable()
+      table.string('avatar', 255).nullable()
 
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
@@ -16,6 +18,7 @@ export default class extends BaseSchema {
   }
 
   async down() {
+    this.schema.raw('DROP EXTENSION IF EXISTS "uuid-ossp"')
     this.schema.dropTable(this.tableName)
   }
 }
---
+++ database/migrations/1722918846614_create_tasks_table.ts
@@ -0,0 +1,23 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'tasks'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').notNullable().primary()
+      table.uuid('project_id').notNullable().references('id').inTable('projects').onDelete('CASCADE')
+      table.uuid('user_id').notNullable().references('id').inTable('users').onDelete('CASCADE')
+      table.string('title').notNullable()
+      table.text('description').nullable()
+      table.boolean('is_completed').defaultTo(false).notNullable()
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
