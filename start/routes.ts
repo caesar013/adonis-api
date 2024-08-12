54de3bf82774d57ac28bd19bb3ca49127153db0a
cαεsαɾ <91421259+caesar013@users.noreply.github.com>
2024-08-12 10:20:09 +0700

    [update] routes

--- start/routes.ts
+++ start/routes.ts
@@ -11,6 +11,10 @@ import router from '@adonisjs/core/services/router'
 const ProjectsController = async () => await import('#controllers/projects_controller')
 const TasksController = async () => await import('#controllers/tasks_controller')
 const UsersController = async () => await import('#controllers/users_controller')
+const UpdateUserNamesController = async () => await import('#controllers/update_user_names_controller')
+const UpdateUserEmailsController = async () => await import('#controllers/update_user_emails_controller')
+const UpdateUserPasswordsController = async () => await import('#controllers/update_user_passwords_controller')
+const UpdateUserAvatarsController = async () => await import('#controllers/update_user_avatars_controller')
 
 router.get('/', async () => {
   return {
@@ -22,4 +26,8 @@ router.group(() => {
   router.resource('/projects', ProjectsController).apiOnly()
   router.resource('/users', UsersController).apiOnly().except(['update'])
   router.resource('/tasks', TasksController).apiOnly()
+  router.patch('/users/:id/update-name', [UpdateUserNamesController])
+  router.patch('/users/:id/update-email', [UpdateUserEmailsController])
+  router.patch('/users/:id/update-password', [UpdateUserPasswordsController])
+  router.patch('/users/:id/update-avatar', [UpdateUserAvatarsController])
 }).prefix('api')
