7c68c24f26d00ed558c0bdcdbe96ee0da0c38316
cαεsαɾ <91421259+caesar013@users.noreply.github.com>
2024-08-17 23:09:40 +0700

    [update] add abilities on update

--- app/controllers/update_user_emails_controller.ts
+++ app/controllers/update_user_emails_controller.ts
@@ -3,11 +3,16 @@ import User from '#models/user'
 import { updateEmailValidator } from '#validators/user'
 
 export default class UpdateUserEmailsController {
-  async handle({ params, request }: HttpContext) {
+  async handle({ params, request, bouncer, response }: HttpContext) {
     // Handle request
     const validated = await request.validateUsing(updateEmailValidator)
     delete validated.old_email
-    const user = (await User.findOrFail(params.id)).merge(validated).save()
-    return user
+    const user = await User.findOrFail(params.id)
+    if (await bouncer.allows('editUser', user)) {
+      user.email = validated?.email || user.email
+      await user.save()
+      return response.status(200).send({ status: true, user: user, message: 'Email updated successfully' })
+    }
+    return response.status(403).send({ status: false, message: 'You are not authorized to edit' })
   }
 }
--- app/controllers/update_user_names_controller.ts
+++ app/controllers/update_user_names_controller.ts
@@ -3,13 +3,18 @@ import { updateNameValidator } from '#validators/user'
 import User from '#models/user'
 
 export default class UpdateUserNamesController {
-  async handle({ params, request, response }: HttpContext) {
+  async handle({ params, request, response, bouncer }: HttpContext) {
     // Handle request
     const validated = await request.validateUsing(updateNameValidator)
     if (validated?.name == null) {
       return response.status(400).send({ status: false, message: 'Validation Failed' })
     }
-    const user = (await User.findOrFail(params.id)).merge(validated).save()
-    return user
+    const user = await User.findOrFail(params.id)
+    if (await bouncer.allows('editUser', user)) {
+      user.name = validated.name
+      await user.save()
+      return response.status(200).send({ status: true, user: user, message: 'Name updated successfully' })
+    }
+    return response.status(403).send({ status: false, message: 'You are not authorized to edit' })
   }
 }
